9c11f3175fca44733e5a717e51a8c1dc26fe03a8
Sujal <001sujalkhatiwada@gmail.com>
2024-07-13 12:41:27 +0545

    Tested receiving form data and responding with html.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 A HTTP server made with raw TCP socket in Python.
-Can accept requests and check if it's a GET request or a POST request.
-Can also view data from POST requests.
-Always responds with an empty page for now.
+Test branch for form data.
--- __main__.py
+++ __main__.py
@@ -25,7 +25,7 @@ def get_route(line:str) -> str:
     route = line.replace(req_type,"").replace("HTTP/1.1","").strip()
     return route
 
-for i in range(4):
+for i in range(3):
     client_sock, client_addr = sock.accept()
     #data comes here in cmd. tf
     msg = client_sock.recv(1024).decode().split(SPLIT) #also splits json data in post
@@ -33,24 +33,42 @@ for i in range(4):
     #print(msg)
     route = get_route(msg[0])
     req_type = get_request_type(msg[0])
-    
+    response = ''
     print(f'Route: {route}')
     if req_type=="GET":
+        with open('form.html') as file:
+            response = file.read()
         print("GET request!")
-    elif req_type=="POST":
-        print("POST request!")
+
+    elif req_type=="POST" and route=='/formtest':
+        print("POST request for /formtest!")
         #data comesd later in powershell
         #temp solution - check if continue is present in header
+        data = []
         if 'Expect: 100-continue' in msg:
             data = client_sock.recv(1024).decode().split(SPLIT)
-            print(data)
         else:
             #if does not have expect, the data is bundled with the 1st request
             #data always comes after ,''. So ,'', '' means no data (usually in get reqs)
             divider = msg.index('') + 1 #+1 because we want the index after empty string
             data = msg[divider:]
+        if not len(data) == 0:
+            data = data[0].split('&')
+            data = [tuple(i.split('=')) for i in data]
             print(data)
-        #need to serialize differently based on Content-Type
 
-    client_sock.sendall(resp().encode())
+            response = f'''
+                <html>
+                <head> <title> Resp </title> </head>
+                <body>
+                    Hello {data[0][1]} {data[1][1]}.<br/>
+                    You have specified your email to be {data[2][1].replace('%40','@')}.<br/>
+                </body>
+                </html>
+            '''
+
+
+        
+
+    client_sock.sendall(resp(response).encode())
     client_sock.close()
